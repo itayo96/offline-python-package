30cfa6d8ff5b6eb018ced0cd5f6b78cb48ddcdd1
Itay Ovadia <itay.ovadi@gmail.com>
2020-12-24 01:45:19 +0200

    - Now the script moves the requirements file to the offline folder
    - Added the offline installation script to the offline folder

--- create_offline_installer.py
+++ create_offline_installer.py
@@ -1,3 +1,4 @@
+import importlib.util
 import os
 import subprocess
 import sys
@@ -6,13 +7,18 @@ import argparse
 
 VIRTUALENV_LIB_NAME = 'virtualenv'
 TEMP_FOLDER_PATH = r'temp_virtual_env'
-REQUIREMENTS_FILE = os.path.join(TEMP_FOLDER_PATH, r'requirements.txt')
+REQUIREMENTS_FILE_NAME = r'requirements.txt'
+TEMP_REQUIREMENTS_FILE = os.path.join(TEMP_FOLDER_PATH, REQUIREMENTS_FILE_NAME)
 VIRTUALENV_PIP = os.path.join(TEMP_FOLDER_PATH, 'Scripts', 'pip')
+OUTPUT_FOLDER_NAME = "offline-{}-installer"
+INSTALLATION_SCRIPT_PATH = os.path.join(OUTPUT_FOLDER_NAME, r'install.py')
+OFFLINE_INSTALLATION_COMMAND = 'import subprocess\nimport sys\nsubprocess.check_call([sys.executable, "-m", "pip", ' \
+                              '"install", "--no-index", "--find-links=./", "-r", "requirements.txt"]) '
 
 
 def create_offline_installer(package_name):
     # Install virtualenv if it's not already installed
-    if VIRTUALENV_LIB_NAME not in sys.modules:
+    if VIRTUALENV_LIB_NAME not in sys.modules and importlib.util.find_spec(VIRTUALENV_LIB_NAME) is None:
         subprocess.check_call([sys.executable, "-m", "pip", "install", VIRTUALENV_LIB_NAME])
 
     # Install the requested package inside the temporary virtualenv
@@ -20,15 +26,20 @@ def create_offline_installer(package_name):
     subprocess.check_call([os.path.join(TEMP_FOLDER_PATH, 'Scripts', 'activate.bat')])
     subprocess.check_call([VIRTUALENV_PIP, "install", package_name])
 
-    with open(REQUIREMENTS_FILE, "w") as requirements_file:
+    with open(TEMP_REQUIREMENTS_FILE, "w") as requirements_file:
         subprocess.check_call([VIRTUALENV_PIP, "freeze"], stdout=requirements_file)
 
     # Create the offline installation folder
-    subprocess.check_call([VIRTUALENV_PIP, "download", "-r", REQUIREMENTS_FILE,
-                           "-d", f"offline-{package_name}-installer"])
-    # TODO Move the requirements file to this folder
+    subprocess.check_call([VIRTUALENV_PIP, "download", "-r", TEMP_REQUIREMENTS_FILE,
+                           "-d", OUTPUT_FOLDER_NAME.format(package_name)])
+    # Move the requirements file to this folder
+    os.replace(TEMP_REQUIREMENTS_FILE, os.path.join(OUTPUT_FOLDER_NAME.format(package_name), REQUIREMENTS_FILE_NAME))
 
-    # TODO Create the installation script for this package
+    # Create the offline installation script for this package
+    with open(INSTALLATION_SCRIPT_PATH.format(package_name), 'w') as script_file:
+        script_file.write(OFFLINE_INSTALLATION_COMMAND)
+
+    # TODO Remove the temporary folder
 
 
 if __name__ == '__main__':
@@ -36,4 +47,4 @@ if __name__ == '__main__':
     parser.add_argument('requested_package', help='The name of the requested package')
     args = parser.parse_args()
 
-    create_offline_installer(args.package_name)
+    create_offline_installer(args.requested_package)
